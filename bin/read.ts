1a184ba206aefe1e3578fe8d7f731dab50efffd5
FukeKazki <kazkichi0906@gmail.com>
2023-02-18 17:38:31 +0900

    json対応

--- bin/read.ts
+++ bin/read.ts
@@ -3,7 +3,6 @@
 import { Command } from "cliffy/command/mod.ts";
 import { Cell, Table } from "cliffy/table/mod.ts";
 import { basename, extname, fromFileUrl, join, normalize } from "std";
-import { parse } from "https://deno.land/std@0.177.0/datetime/mod.ts";
 
 const displayReadingList = async () => {
   // どこから実行しても ~/readinglist/を参照できるように
@@ -16,42 +15,45 @@ const displayReadingList = async () => {
   for await (const dirEntry of Deno.readDir(path)) {
     if (dirEntry.isDirectory) continue;
     if (dirEntry.name === "README.md") continue;
+    if (extname(dirEntry.name) === ".md") continue;
     files.push(path + dirEntry.name);
   }
 
   // 出力用のテーブルを作成
   const table: Table = new Table()
-    .header(["date", "title", "url"])
+    .header(["date", "title", "url", "memo"])
     .body([])
     .border(true)
+    .maxColWidth(100)
     .sort();
 
   // 日付け順にソート
   files.sort();
+
+  type JSON = [{
+    date: string;
+    title: string;
+    url: string;
+    memo: string;
+  }];
   // titleとurlを取り出す
   for (const file of files) {
     const res = await Deno.readTextFile(file);
-    // ファイル名から日付を取り出す
-    const dateMatcher =
-      /(?<year>[0-9]{4})(?<month>[0-9]{2})(?<date>[0-9]{2}).md/;
-    const { year, month, date } = basename(file).match(dateMatcher)?.groups;
-
-    const line = res.split("\n");
-    line.map((str, index) => {
-      if (str === "") return;
-      const urlMatcher = /(?<url>https?:\/\/[\w/:%#\$&\?~\.=\+\-]+)/;
-      const url = str.match(urlMatcher)?.groups?.url;
-      const titleMatcher = /\[(?<title>.+)\]/;
-      const title = str.match(titleMatcher)?.groups?.title;
-      if (!url || !title) return;
+    const json = JSON.parse(res) as JSON;
+    console.log(json);
+    json.map((obj, index) => {
+      const url = obj.url;
+      const title = obj.title;
+      const date = obj.date;
       const rows = [];
       if (index === 0) {
         rows.push(
-          new Cell(`${year}/${month}/${date}`).rowSpan(line.length - 1),
+          new Cell(date).rowSpan(json.length),
         );
       }
       rows.push(title);
       rows.push(url);
+      rows.push(obj.memo);
       table.push(rows);
     });
   }
