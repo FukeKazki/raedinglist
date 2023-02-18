6dcbfcb961a7dbacc090f8c87bf90dae9b3aaff2
FukeKazki <kazkichi0906@gmail.com>
2023-02-18 17:28:00 +0900

    update

--- bin/read.ts
+++ bin/read.ts
@@ -2,7 +2,8 @@
 
 import { Command } from "cliffy/command/mod.ts";
 import { Cell, Table } from "cliffy/table/mod.ts";
-import { basename, fromFileUrl, join, normalize } from "std";
+import { basename, extname, fromFileUrl, join, normalize } from "std";
+import { parse } from "https://deno.land/std@0.177.0/datetime/mod.ts";
 
 const displayReadingList = async () => {
   // どこから実行しても ~/readinglist/を参照できるように
@@ -66,6 +67,54 @@ const pullReadingList = async () => {
   p.close();
 };
 
+const convert = async () => {
+  const filePath = fromFileUrl(import.meta.url);
+  const path = normalize(join(filePath, "../", "../"));
+
+  const files: string[] = [];
+
+  // "yyyymmdd.md"なファイル取り出す
+  for await (const dirEntry of Deno.readDir(path)) {
+    if (dirEntry.isDirectory) continue;
+    if (dirEntry.name === "README.md") continue;
+    if (extname(dirEntry.name) === ".json") continue;
+    files.push(path + dirEntry.name);
+  }
+  console.log(files);
+  for (const file of files) {
+    const res = await Deno.readTextFile(file);
+    const dateMatcher =
+      /(?<year>[0-9]{4})(?<month>[0-9]{2})(?<date>[0-9]{2}).md/;
+    const { year, month, date } = basename(file).match(dateMatcher)?.groups;
+    const path = `${year}${month}${date}.json`;
+
+    const list: any[] = [];
+    const line = res.split("\n");
+    line.map((str) => {
+      if (str === "") return;
+      const urlMatcher = /(?<url>https?:\/\/[\w/:%#\$&\?~\.=\+\-]+)/;
+      const url = str.match(urlMatcher)?.groups?.url;
+      const titleMatcher = /\[(?<title>.+)\]/;
+      const title = str.match(titleMatcher)?.groups?.title;
+      if (!url || !title) return;
+      list.push({
+        date: `${year}-${month}-${date}`,
+        title,
+        url,
+        memo: "",
+      });
+    });
+
+    await Deno.writeTextFile(
+      path,
+      JSON.stringify(list),
+      {
+        create: true,
+      },
+    );
+  }
+};
+
 await new Command()
   .name("reading")
   .version("0.1.0")
@@ -73,4 +122,6 @@ await new Command()
   .action((_options, ..._args) => displayReadingList())
   .command("update", "Update command.")
   .action((_option, ..._args) => pullReadingList())
+  .command("convert", "markdown convert json")
+  .action((_option, ..._args) => convert())
   .parse(Deno.args);
